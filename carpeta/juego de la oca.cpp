1d084636275c922eb6fe7f10e0e04acaf78e1d39
N3STOR173 <nestorgmayor@gmai.com>
2022-10-19 17:42:09 +0200

    texto de ejemplo

--- carpeta/juego de la oca.cpp
+++ carpeta/juego de la oca.cpp
@@ -156,7 +156,7 @@ int tirarDadoManual(){
 int efectoPosicion(int casillaActual){
     if (esOca (casillaActual)){
         cout << "De Oca en Oca y tiro porque me toca\n";
-        cout << "Pasas a la casilla: " << siguienteOca(casillaActual) << "\n Y vuelves a tirar";
+        cout << "Pasas a la casilla: " << siguienteOca(casillaActual) << "\nY vuelves a tirar\n";
         return siguienteOca(casillaActual);
 
     }
@@ -166,14 +166,9 @@ int efectoPosicion(int casillaActual){
 
 int efectoTiradas(int casillaActual, int numeroDeTiradas){
     if (esOca (casillaActual)){
-
+        return numeroDeTiradas + 1;
     }
-
-
 }
-
-
-
 
 int siguienteOca(int casilla){
     if (casilla % 9 == 0)
